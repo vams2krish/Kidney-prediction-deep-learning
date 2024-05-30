93b61487375e0dce9eac1c94141d948d3b7a7561
Vamshikrishna <46240162+vams2krish@users.noreply.github.com>
2024-05-30 17:02:39 -0500

    Error fix in model training

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from cnnClassifer import logger
 from cnnClassifer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifer.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifer.pipeline.stage_03_model_training import ModelTrainingPipeline
+
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -23,6 +25,20 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
